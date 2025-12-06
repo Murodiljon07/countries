83bca0773483bb6ead1f71e258c6098014b013f1
Murodiljon07 <murodiljonabdumutalov.com>
2025-12-07 03:48:17 +0500

    first commit

---
+++ date.js
@@ -0,0 +1,61 @@
+const countries = [
+  {
+    flag: "./pictures/Germany.png",
+    name: "Germany",
+    population: "81,770,900",
+    region: "Europe",
+    capital: "Berlin",
+  },
+  {
+    flag: "./pictures/flag-usa.png",
+    name: "United States of America",
+    population: "323,947,000",
+    region: "Americas",
+    capital: "Washington, D.C.",
+  },
+
+  {
+    flag: "./pictures/Brazil.png",
+    name: "Brazil",
+    population: "206,135,893",
+    region: "Americas",
+    capital: "Brasília",
+  },
+  {
+    flag: "./pictures/iceland.png",
+    name: "Iceland",
+    population: "334,300",
+    region: "Europe",
+    capital: "Reykjavík",
+  },
+  {
+    flag: "./pictures/afganistan.png",
+    name: "Afghanistan",
+    population: "27,657,145",
+    region: "Asia",
+    capital: "Kabul",
+  },
+  {
+    flag: "./pictures/aland.png",
+    name: "Åland Islands",
+    population: "28,875",
+    region: "Europe",
+    capital: "Mariehamn",
+  },
+  {
+    flag: "./pictures/albania.png",
+    name: "Albania",
+    population: "2,886,026",
+    region: "Europe",
+    capital: "Tirana",
+  },
+  {
+    flag: "./pictures/algeria.png",
+    name: "Algeria",
+    population: "40,400,000",
+    region: "Africa",
+    capital: "Algiers",
+  },
+];
+
+export default countries;
